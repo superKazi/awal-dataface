89ab6e136604979d1053fed1893920e689f3d6cd
Kazi Awal <kazi.digs@gmail.com>
2025-06-19 09:40:06 -0400

    static

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,6 +3,8 @@ import type { LayoutServerLoad } from './$types';
 import type { Procedure } from '$lib/types';
 import labelcsv from '$lib/data/labels.csv?raw';
 
+export const prerender = true;
+
 export const load: LayoutServerLoad = async () => {
 	let procedures: Procedure[] = csvParse(labelcsv);
 	return {
